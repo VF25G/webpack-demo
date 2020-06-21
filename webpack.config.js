69d6487f013c37c9585bf4f04d3700d45ace57a2
MiniCongee <replybber@gmail.com>
2020-06-21 21:45:44 +0800

    learned Webpack JS HTML CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'BangDream',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 }
