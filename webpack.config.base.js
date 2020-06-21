7b7f44c36e0341f7f4e3e71c9457262932100064
MiniCongee <replybber@gmail.com>
2020-06-21 22:43:36 +0800

    learned use two config

---
+++ webpack.config.base.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BangDream',
+            template: 'src/assets/index.html'
+        })
+    ]
+}
