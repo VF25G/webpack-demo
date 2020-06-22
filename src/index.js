46cfd53e1510653b016cca3ad1e0c4ee020fb635
MiniCongee <replybber@gmail.com>
2020-06-22 16:28:30 +0800

    Lazy loader

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ const div = document.getElementById('app')
 div.innerHTML = `
   <img src="${bangDream}">
 `
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy')
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log('模块加载错误')
+    })
+}
+div.appendChild(button)
